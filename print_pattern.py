c3b1b63253f32a782e86087a806bacffb3e5a206
adatta2025 <arijit.datta2006@gmail.com>
2025-04-23 17:52:45 +0530

    Changed character to be printed from $ to #. Added line separator at the end of program

--- print_pattern.py
+++ print_pattern.py
@@ -2,7 +2,7 @@ print('\nProgram to Print Character Patterns\n')
 print("\n====================================\n")
 num_line_top = 7
 num_line_bot = 6
-ch_p = '$'
+ch_p = '#'
 for num in range(1, num_line_top):
     for num0 in range(1, num_line_top-num):
         print(' ', end=' ')
@@ -15,3 +15,4 @@ for num in range(1, num_line_bot):
     for num1 in range(1, (num_line_bot-num)*2):
         print(ch_p, end=' ')
     print('\n')
+print("\n====================================\n")
